d924e5619d60df4691e954377d6caf2e73f73891
NiksonGlikson <hermesshant@gmail.com>
2022-05-18 13:07:24 +0300

    update4

--- backend/app.js
+++ backend/app.js
@@ -35,21 +35,20 @@ const allowedCors = [
   "https://praktikum.tk",
   "http://praktikum.tk",
   "localhost:3000",
+  "http://localhost:3000",
   "https://localhost:3000",
+  "https://51.250.74.127:3000",
   "https://51.250.74.127:3000",
   "https://domainname.glinkin.nomoredomains.xyz",
-  "https://localhost:3000",
-  "https://51.250.74.127:3000",
   "http://domainname.glinkin.nomoredomains.xyz",
-  "http://localhost:3000"
 ];
 
 app.use((req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "*");
+    res.header("Access-Control-Allow-Origin", "origin");
+    res.header("Access-Control-Allow-Credentials", "true");
   }
 
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
