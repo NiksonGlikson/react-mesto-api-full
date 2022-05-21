24b39ce7667e331f9fc56a41147225d03c6c485b
NiksonGlikson <hermesshant@gmail.com>
2022-05-21 15:12:25 +0300

    add cors33

--- backend/app.js
+++ backend/app.js
@@ -45,7 +45,7 @@ const allowedCors = [
 app.use(function (req, res, next) {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Credentials", true);
   }
 
